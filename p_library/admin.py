ad77989d2b71bd91ac191e067d159158cc34b495
Yury Shashurin <shashurin.edu@mail.ru>
2020-12-01 00:44:06 +0300

    Final homework

--- p_library/admin.py
+++ p_library/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from p_library.models import Book, Author, Publisher
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    @staticmethod
+    def author_full_name(obj):
+        return obj.author.full_name
+
+    list_display = ('title', 'author_full_name', 'copy_count', 'publisher')
+    fields = ('ISBN', 'title', 'description', 'year_release', 'author', 'price','copy_count','publisher')
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    @staticmethod
+    def author_full_name(obj):
+        return obj.full_name
+
+    list_display = ('author_full_name','birth_year', 'country')
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('title','country')
--- p_library/views.py
+++ p_library/views.py
@@ -1,9 +1,67 @@
 from django.shortcuts import render
-from .models import Book, Author
+from p_library.models import Book, Author, Publisher
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
 
 
 # Create your views here.
 def books_list(request):
     books = Book.objects.all()
-    return HttpResponse(books)
+    book_lists = []
+    for book in books:
+        book_lists.append(book.title)
+
+    return HttpResponse(book_lists)
+
+def index(request):
+    template = loader.get_template('index.html')
+    books = Book.objects.all()
+    biblio_data = {
+        "title": "мою библиотеку",
+        "books": books,
+    }
+    return HttpResponse(template.render(biblio_data, request))
+
+def publishers(request):
+    template = loader.get_template('publishers.html')
+    publishers = Publisher.objects.all()
+    publisher_data = {
+        'publishers': publishers
+    }
+
+    return HttpResponse(template.render(publisher_data, request))
+
+def book_increment(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            book.copy_count += 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
+
+
+def book_decrement(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            if book.copy_count < 1:
+                book.copy_count = 0
+            else:
+                book.copy_count -= 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
